7557c5ea7a4d0eb1aee3e49bb44501a833cae2fb
Nikhil-Varanasi <nikhilvaranasi111@gmail.com>
2024-12-28 15:47:05 +0530

    few syntex changes

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "vpc_id" {
-    value   = aws_vpc.main.id   
+  value = aws_vpc.main.id
 }
 
 output "az_info" {
-    value = data.aws_availability_zones.available
+  value = data.aws_availability_zones.available
 }
--- variables.tf
+++ variables.tf
@@ -1,77 +1,77 @@
 
 variable "project_name" {
-    type = string
+  type = string
 }
 variable "environment" {
-    type = string
+  type = string
 }
 
 variable "vpc_cidr" {
   default     = "10.0.0.0/16"
   description = "description"
 }
+
 variable "enable_dns_hostnames" {
-    default = true
+  default = true
 }
 
 
 #optional
 variable "common_tags" {
-    default = {
-    }
+  default = {}
 }
 
 variable "vpc_tags" {
-    default = {}
+  default = {}
 }
 
 variable "igw_tags" {
-    default = {}
+  default = {}
 }
 
 variable "public_subnet_cidrs" {
-    type = list
-    #we need to write condition to make sure exactly 2 are given(size validation)
-    validation {
-        condition = length(var.public_subnet_cidrs) == 2
-        error_message = "please provide 2 valid public subnet CIDR"
-    }
+  type = list(any)
+  #we need to write condition to make sure exactly 2 are given(size validation)
+  validation {
+    condition     = length(var.public_subnet_cidrs) == 2
+    error_message = "please provide 2 valid public subnet CIDR"
+  }
 }
 
 variable "public_subnet_tags" {
-    default = {}
-} 
+  default = {}
+}
 
 variable "private_subnet_cidrs" {
-    type = list
-    #we need to write condition to make sure exactly 2 are given(size validation)
-    validation {
-        condition = length(var.private_subnet_cidrs) == 2
-        error_message = "please provide 2 valid private subnet CIDR"
-    }
+  type = list(any)
+  #we need to write condition to make sure exactly 2 are given(size validation)
+  validation {
+    condition     = length(var.private_subnet_cidrs) == 2
+    error_message = "please provide 2 valid private subnet CIDR"
+  }
 }
 
 variable "private_subnet_tags" {
-    default = {}
-} 
+  default = {}
+}
 
 variable "database_subnet_cidrs" {
-    type = list
-    #we need to write condition to make sure exactly 2 are given(size validation)
-    validation {
-        condition = length(var.database_subnet_cidrs) == 2
-        error_message = "please provide 2 valid database subnet CIDR"
-    }
+  type = list(any)
+  #we need to write condition to make sure exactly 2 are given(size validation)
+  validation {
+    condition     = length(var.database_subnet_cidrs) == 2
+    error_message = "please provide 2 valid database subnet CIDR"
+  }
 }
 
 variable "database_subnet_tags" {
-    default = {}
+  default = {}
 }
 
 variable "db_subnet_group_tags" {
-    default = {}
+  default = {}
 }
 
 variable "nat_gateway_tags" {
-    default = {}
+  default = {}
 }
